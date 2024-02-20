1614835c2f3fde83f9b73ee35600571b0ac11937
Paramdeep Singh <prmdpsn56@gmail.com>
2024-02-19 21:31:26 -0800

    fixed details page

--- src/components/ExersiseDetails.tsx
+++ src/components/ExersiseDetails.tsx
@@ -10,7 +10,7 @@ import Loader from './Loader'
 
 type Props = {}
 
-type Exercise = {
+interface Exercise {
         bodyPart?: string,
         equipment?:string,
         gifUrl?:string,
@@ -27,75 +27,85 @@ type Videos = {
     next?: string
 }
 
+
+type RouteParams ={
+    id: string; 
+  }
+
 const ExersiseDetails = (props: Props) => {
    const [exerciseInformation,setExerciseInformation] = useState<Exercise>({})
    const [exerciseVideos, setExerciseVideos] = useState<Videos>({})
    const [targetMuscleExercises, setTargetMuscleExercises] = useState<any[]>([]);
    const [equipmentExercises, setEquipmentExercises] = useState<any[]>([]);
+   
    const [state,setState] = useState(false);
-
-   const {id} = useParams();
-
+   const [anotherState,setAnotherState] = useState("");
+   const {id} = useParams<RouteParams>();
+    console.log(id);
 
    useEffect(()=>{
         const fetchExercisesdate = async () => {
             const exerciseDbUrl = `https://exercisedb.p.rapidapi.com`
             const youtubeSearchUrl = `https://youtube-search-and-download.p.rapidapi.com`;
-
-
-            setTimeout(()=>{
-                setState(true);
-            },5000)
-            //Store logic to be updated
-            // let exercisesFromStorage = JSON.parse(localStorage.getItem(`${id}`) || '{}');
-            // let videosFromStorage = JSON.parse(localStorage.getItem(`${id}_video`) || '{}');
-            // let similarFromStorage = JSON.parse(localStorage.getItem(`${id}_muscle`) || '{}');
-            // let equipmentFromStorage = JSON.parse(localStorage.getItem(`${id}_equipment`) || '{}');
             
 
-            // if(exercisesFromStorage.bodyPart && videosFromStorage.contents && similarFromStorage && equipmentFromStorage){
-            //     setExerciseDetail(exercisesFromStorage);
-            //     setExerciseVideos(videosFromStorage);
-            //     setTargetMuscleExercises(similarFromStorage);
-            //     setEquipmentExercises(equipmentFromStorage);
-            // }else{
+            // setTimeout(()=>{
+            //     console.log('setState Running');
+            //     setState(true);
+            //     setAnotherState("true")
+            // },4000)
+            //Store logic to be updated
+
+            let exercisesFromStorage = JSON.parse(localStorage.getItem(id) || '{}');
+            console.log(exercisesFromStorage);
+            let videosFromStorage = JSON.parse(localStorage.getItem(`${id}_video`) || '{}');
+            let similarFromStorage = JSON.parse(localStorage.getItem(`${id}_muscle`) || '{}');
+            let equipmentFromStorage = JSON.parse(localStorage.getItem(`${id}_equipment`) || '{}');
+            
+
+            if(exercisesFromStorage.bodyPart && videosFromStorage.contents && similarFromStorage && equipmentFromStorage){
+                setExerciseInformation(exercisesFromStorage);
+                setExerciseVideos(videosFromStorage);
+                setTargetMuscleExercises(similarFromStorage);
+                setEquipmentExercises(equipmentFromStorage);
+            }else{
                 console.log(!!exerciseInformation.bodyPart);
                 const exerciseDetailData  = await ExerciseServices(`${exerciseDbUrl}/exercises/exercise/${id}`,ExerciseOptions);
-                // const exerciseVideosData = await ExerciseServices(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,videoOptions);
-                // const targetMuscleExerciseData = await ExerciseServices(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`,ExerciseOptions);
-                // const equipmentExerciseData = await ExerciseServices(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`,ExerciseOptions);
+                const exerciseVideosData = await ExerciseServices(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,videoOptions);
+                const targetMuscleExerciseData = await ExerciseServices(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`,ExerciseOptions);
+                const equipmentExerciseData = await ExerciseServices(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`,ExerciseOptions);
 
                 console.log(exerciseDetailData);
-                // console.log(exerciseVideosData);
-                // console.log(targetMuscleExerciseData);
-                // console.log(equipmentExerciseData);
-                // console.log(exerciseVideosData)
-                // localStorage.setItem(`${id}`,JSON.stringify(exerciseDetailData));
-                // localStorage.setItem(`${id}_video`,JSON.stringify(exerciseVideosData));
-                // localStorage.setItem(`${id}_muscle`,JSON.stringify(targetMuscleExerciseData));
-                // localStorage.setItem(`${id}_equipment`,JSON.stringify(equipmentExerciseData));
+                console.log(exerciseVideosData);
+                console.log(targetMuscleExerciseData);
+                console.log(equipmentExerciseData);
+                localStorage.setItem(`${id}`,JSON.stringify(exerciseDetailData));
+                localStorage.setItem(`${id}_video`,JSON.stringify(exerciseVideosData));
+                localStorage.setItem(`${id}_muscle`,JSON.stringify(targetMuscleExerciseData));
+                localStorage.setItem(`${id}_equipment`,JSON.stringify(equipmentExerciseData));
 
-                // setExerciseVideos(exerciseVideosData)
+                setExerciseVideos(exerciseVideosData)
                 setExerciseInformation(exerciseDetailData);
 
 
                 console.log(!!exerciseInformation);
-                // setTargetMuscleExercises(targetMuscleExerciseData);
-                // setEquipmentExercises(equipmentExerciseData);
-                //REMOVE CONSOLE
+                setTargetMuscleExercises(targetMuscleExerciseData);
+                setEquipmentExercises(equipmentExerciseData);
+                // REMOVE CONSOLE
                 // console.log(exerciseDetailData)
             }
-        // }
+            
+        }
         fetchExercisesdate();
-   },[id])
-
+   },[])
+   
   return (
     <div>
-        {!!exerciseInformation.bodyPart ? <p>Hello</p>: <Loader></Loader>}
+        {/* {state && <p>Hello</p>} */}
+        <p>{anotherState}</p>
         {!!exerciseInformation.bodyPart ? <Detail exerciseDetails={exerciseInformation} ></Detail>: <Loader/>}
-        {/* {exerciseInformation ? <ExerciseVideos exerciseVideos={exerciseVideos.contents} exerciseName={exerciseInformation.name}/>: <Loader/>} */}
-        {/* <ExerciseVideos exerciseVideos={exerciseVideos.contents} exerciseName={exerciseDetail.name}/>
-        <SimilarExercises similarMuscle={targetMuscleExercises} similarEquipment={equipmentExercises} /> */}
+        {exerciseInformation.bodyPart ? <ExerciseVideos exerciseVideos={exerciseVideos.contents} exerciseName={exerciseInformation.name}/>: <Loader/>}
+        {exerciseInformation.bodyPart && <SimilarExercises similarMuscle={targetMuscleExercises} similarEquipment={equipmentExercises} />}
     </div>
 
   )
